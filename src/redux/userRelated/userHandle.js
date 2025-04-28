164ccfdd48a3a18eff3b5c6247acf67541223886
Atharve Agrawal <atharve211@gmail.com>
2025-04-28 22:14:24 +0530

    live backend attached

--- src/redux/userRelated/userHandle.js
+++ src/redux/userRelated/userHandle.js
@@ -13,7 +13,7 @@ import {
     getError,
 } from './userSlice';
 
-const REACT_APP_BASE_URL = "http://localhost:8080";
+const REACT_APP_BASE_URL = "https://eduvise.onrender.com";
 
 export const loginUser = (fields, role) => async (dispatch) => {
     dispatch(authRequest());
