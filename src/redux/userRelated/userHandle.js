888933952aa8d0e66f00cb6d462774e71b2421bc
Atharve Agrawal <atharve211@gmail.com>
2025-05-24 00:14:31 +0530

    weekly commits

--- src/redux/userRelated/userHandle.js
+++ src/redux/userRelated/userHandle.js
@@ -13,7 +13,7 @@ import {
     getError,
 } from './userSlice';
 
-const REACT_APP_BASE_URL = "https://eduvise-server.onrender.com";
+const REACT_APP_BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const loginUser = (fields, role) => async (dispatch) => {
     dispatch(authRequest());
